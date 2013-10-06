5cb3602f5a5cb05e4ab3c1752381108b3fbe3cb0
Jon Saints <saintsjd@gmail.com>
2013-10-06 17:14:24 -0600

    sendgrid send

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
 import os
 from flask import Flask, request
+import sendgrid 
 
 app = Flask(__name__)
-MAILGUN_API_KEY = os.getenv('MAILGUN_API_KEY',False)
+SENDGRID_USERNAME = os.getenv('SENDGRID_USERNAME',False)
+SENDGRID_PASSWORD = os.getenv('SENDGRID_PASSWORD',False)
+
 
 @app.route('/')
 def hello():
-    return 'Notification app for Dsenyo.com'
+	return 'Notification app for Dsenyo.com'
 
 @app.route('/customer/new', methods=['POST'])
 def customer_new():
-    app.logger.error(request) 
+	customer = request.json
+	s = sendgrid.Sendgrid(SENDGRID_USERNAME, SENDGRID_PASSWORD, secure=True)
+	subject = "[Dsenyo Notifications] New Wholesale Customer: {} {}".format(customer.first_name, customer.last_name)
+
+	body = """A new customer account was created on the wholesale store.
+
+Name: {} {}
+Email: {}
+Notes: {}
+	""".format(customer.first_name, customer.last_name, customer.email, customer.note)
+	# make a message object
+	message = sendgrid.Message("notifications@dsenyo.com", subject, body, None )
+	# add a recipient
+	message.add_to("ashley@dsenyo.com", "Ashley Rein")
+	message.add_to("marissa@dsenyo.com", "Marissa Saints")
+	# use the Web API to send your message
+	s.smtp.send(message)	
     return 'OK'
+
+if __name__ == '__main__':
+    app.run(debug=True)
