ee7e790ec7e3491770d946b64478d7ec4c41786f
Jon Saints <saintsjd@gmail.com>
2013-10-06 10:54:55 -0600

    test webhook from shopify

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+MAILGUN_API_KEY = os.getenv('MAILGUN_API_KEY',False)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'Notification app for Dsenyo.com'
+
+@app.route('/customer/new', methods=['POST'])
+def customer_new():
+    app.logger.debug(request) 
