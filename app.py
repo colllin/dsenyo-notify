e6a3ec33543aacbf6c6dcd4e71e2f2a3a3ae3bb5
Jon Saints <saintsjd@gmail.com>
2015-03-02 13:28:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def customer_new():
 	# make a message object
 	message = sendgrid.Message("notifications@dsenyo.com", subject, body, None )
 	# add a recipient
-	message.add_to("team@dsenyo.com", "Amy Travis")
+	message.add_to("team@dsenyo.com", "Dsenyo Team")
 	#message.add_to("marissa@dsenyo.com", "Marissa Saints")
 	#message.add_to("saintsjd@gmail.com", "Jon Saints")
 	# use the Web API to send your message
