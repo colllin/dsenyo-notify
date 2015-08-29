80d9c84016fb999ffb842dac5dcc6b95c92da017
Jon Saints <saintsjd@gmail.com>
2015-08-29 07:39:28 -0600

    change emails

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request
-import sendgrid 
+import sendgrid
 
 app = Flask(__name__)
 SENDGRID_USERNAME = os.getenv('SENDGRID_USERNAME',False)
@@ -29,13 +29,13 @@ def customer_new():
 	# add a recipient
 	#message.add_to("team@dsenyo.com", "Dsenyo Team")
 	message.add_to("marissa@dsenyo.com", "Marissa Saints")
-	message.add_to("amy@dsenyo.com", "Amy Travis")
+	#message.add_to("amy@dsenyo.com", "Amy Travis")
 	#message.add_to("linda@dsenyo.com", "Linda Xiong")
-	#message.add_to("ashley@dsenyo.com", "Ashley Lestor")
+	message.add_to("ashley@dsenyo.com", "Ashley Lestor")
 	#message.add_to("saintsjd@gmail.com", "Jon Saints")
 	# use the Web API to send your message
 	print customer['first_name'], customer['last_name']
-	s.smtp.send(message)	
+	s.smtp.send(message)
 	return 'OK'
 
 if __name__ == '__main__':
